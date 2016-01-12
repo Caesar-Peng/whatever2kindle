300c6fae30f85e73366335391c4e000d58db38ed
Caesar <caesarpy@gmail.com>
2016-01-12 15:06:13 -0500

    Added some logs.

--- server/src/main/java/com/molecode/w2k/resources/EvernoteResource.java
+++ server/src/main/java/com/molecode/w2k/resources/EvernoteResource.java
@@ -7,6 +7,8 @@ import com.molecode.w2k.fetcher.evernote.EvernoteClient;
 import com.molecode.w2k.fetcher.evernote.EvernoteClientManager;
 import com.molecode.w2k.services.ArticleTransferService;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
@@ -23,6 +25,8 @@ import javax.ws.rs.core.MediaType;
 @Path("/evernote")
 @Component
 public class EvernoteResource {
+
+	private static final Logger LOG = LoggerFactory.getLogger(EvernoteResource.class);
 
 	private static final String RESPONSE_JSON = "{\"status\":\"OK\",\"message\":\"Notification received, thanks.\"}";
 
@@ -47,11 +51,13 @@ public class EvernoteResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String receivedNotification(@QueryParam("userId") String username, @QueryParam("notebookGuid") String notebookGuid,
 			@QueryParam("guid") String noteGuid, @QueryParam("reason") String reason) {
+		LOG.info("Received evernote webhook notification, username: {}, noteGuid: {}, reason :{}", username, noteGuid, reason);
 		if (StringUtils.equalsIgnoreCase("create", reason)) {
 			EvernoteClient evernoteClient = evernoteClientManager.getEvernoteClient(username);
 			if (evernoteClient != null) {
 				ArticleFetcher articleFetcher = new EvernoteArticleFetcher(username, noteGuid, evernoteClient, new ArticleWriter());
 				articleTransferService.transferAndDeliverArticle(articleFetcher);
+				LOG.info("Successfully received and handed evernote webhook notification to ArticleTransferService: {}, {}", username, noteGuid);
 			}
 		}
 		return RESPONSE_JSON;
--- server/src/main/java/com/molecode/w2k/services/impl/ArticleTransferServiceImpl.java
+++ server/src/main/java/com/molecode/w2k/services/impl/ArticleTransferServiceImpl.java
@@ -4,7 +4,10 @@ import com.molecode.w2k.fetcher.ArticleFetcher;
 import com.molecode.w2k.kindle.KindleGenerator;
 import com.molecode.w2k.services.ArticleTransferService;
 import com.molecode.w2k.services.EmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 
 import java.io.File;
 
@@ -13,14 +16,18 @@ import java.io.File;
  */
 public class ArticleTransferServiceImpl implements ArticleTransferService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ArticleTransferServiceImpl.class);
+
 	private KindleGenerator kindleGenerator;
 
 	private EmailService emailService;
 
+	@Required
 	public void setKindleGenerator(KindleGenerator kindleGenerator) {
 		this.kindleGenerator = kindleGenerator;
 	}
 
+	@Required
 	public void setEmailService(EmailService emailService) {
 		this.emailService = emailService;
 	}
@@ -29,6 +36,7 @@ public class ArticleTransferServiceImpl implements ArticleTransferService {
 	public void transferAndDeliverArticle(ArticleFetcher articleFetcher) {
 		File originalFile = articleFetcher.fetchArticle();
 		if (originalFile != null) {
+			LOG.info("Successfully fetched article content with ArticleFetcher.");
 			File kindleFile = kindleGenerator.generate(originalFile);
 			if (kindleFile != null) {
 				emailService.deliverArticle(kindleFile);
