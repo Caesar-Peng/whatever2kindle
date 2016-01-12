50a46525494a90026c6ac533511b262b62032c82
Caesar <caesarpy@gmail.com>
2016-01-12 15:08:27 -0500

    Created an empty implementation for EmailService.

---
+++ server/src/main/java/com/molecode/w2k/services/impl/EmailServiceImpl.java
@@ -0,0 +1,14 @@
+package com.molecode.w2k.services.impl;
+
+import com.molecode.w2k.services.EmailService;
+
+import java.io.File;
+
+/**
+ * Created by YP on 2016-01-12.
+ */
+public class EmailServiceImpl implements EmailService {
+	@Override public void deliverArticle(File kindleFile) {
+
+	}
+}
