c8a6d25227b1840c2d8643259cfac3c5868b7eed
Caesar <caesarpy@gmail.com>
2016-01-18 11:37:13 -0500

    Implemented the kindleGenerator. Made it call kindlegen to convert html files to mobi files.

--- server/src/main/java/com/molecode/w2k/kindle/KindleGenerator.java
+++ server/src/main/java/com/molecode/w2k/kindle/KindleGenerator.java
@@ -1,12 +1,36 @@
 package com.molecode.w2k.kindle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by YP on 2015-12-28.
  */
 public class KindleGenerator {
+
+	private static final Logger LOG = LoggerFactory.getLogger(KindleGenerator.class);
+
 	public File generate(File originalFile) {
-		return null;
+		File kindleFile = null;
+		try {
+			String originalFilePath = originalFile.getAbsolutePath();
+			int exitValue = Runtime.getRuntime().exec("kindlegen " + originalFilePath).waitFor();
+			if (exitValue < 2) {
+				kindleFile = new File(originalFilePath.substring(0, originalFilePath.lastIndexOf('.')) + ".mobi");
+				if (exitValue == 1) {
+					LOG.info("Mobi file generated with warnings: {}" + originalFilePath);
+				}
+			} else {
+				LOG.warn("Failed to generate mobi file: {}" + originalFilePath);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return kindleFile;
 	}
 }
